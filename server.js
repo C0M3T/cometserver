694c4982fe2cfa50b7a54d702c4714f03009a346
Miguel Dominguez <migydgz@gmail.com>
2017-08-01 14:17:31 -0700

    ugh

--- server.js
+++ server.js
@@ -1,130 +1,94 @@
-// Set up
-var express  = require('express');
-var app      = express();
-var mongoose = require('mongoose');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var cors = require('cors');
- 
-// Configuration
-mongoose.connect('mongodb://Abelchalla:Abel323447@ds121543.mlab.com:21543/heroku_bqhtlgll');
- 
-app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
-app.use(bodyParser.json()); // Send JSON responses
-app.use(logger('dev')); // Log requests to API using morgan
+var express = require("express");
+var mongodb = require("mongodb");
+var logger = require("morgan")
+var bodyparser = require("body-parser");
+var cors = require("cors");
+
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+var app = express();
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+app.use(logger('dev'));
 app.use(cors());
- 
-// Models
-var Machine = mongoose.model('Machine', {
-    machine_number: Number,
-    type: String,
-    reserved: [
-        {
-            from: String,
-            to: String
-        }
-    ]
-});
- 
-/*
- * Generate some test data, if no records exist already
- * MAKE SURE TO REMOVE THIS IN PROD ENVIRONMENT
-*/
- 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
- 
-Machine.remove({}, function(res){
-    console.log("removed records");
-});
- 
-Machine.count({}, function(err, count){
-    console.log("Machines: " + count);
- 
-    if(count === 0){
- 
-        var recordsToGenerate = 150;
- 
-        var machineTypes = [
-            'standard',
-            'villa',
-            'penthouse',
-            'studio'
-        ];
- 
-        // For testing purposes, all machines will be booked out from:
-        // 18th May 2017 to 25th May 2017, and
-        // 29th Jan 2018 to 31 Jan 2018
- 
-        for(var i = 0; i < recordsToGenerate; i++){
-            var newMachine = new Machine({
-                machine_number: i,
-                type: machineTypes[getRandomInt(0,3)],
-                beds: getRandomInt(1, 6),
-                max_occupancy: getRandomInt(1, 8),
-                cost_per_night: getRandomInt(50, 500),
-                reserved: [
-                    {from: '1970-01-01', to: '1970-01-02'},
-                    {from: '2017-04-18', to: '2017-04-23'},
-                    {from: '2018-01-29', to: '2018-01-30'}
-                ]
-            });
- 
-            newMachine.save(function(err, doc){
-                console.log("Created test document: " + doc._id);
-            });
-        } 
- 
+
+var client = mongodb.MongoClient;
+var orders;
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+
+client.connect(url, function (err, db) {
+    if (err) {
+        console.log("error connecting");
+        process.exit(1);
+        throw err;
+    } else {
+        console.log("connected to our database")
+        orders = db.collection("orders");
     }
+})
+
+
+// app.get("/", function (req, res) {
+//     console.log("inserting a new event");
+//     orders.insert({
+//         "name": "art exhbit A",
+//         "date": "tommorow"
+//     }, function (err, doc) {
+
+//     });
+
+// })
+
+// app.get("/pullOrders", function (req, res) {
+//     orders.find().toArray(function (err, docs) {
+//         if (err) {
+//             throw err;
+//             res.sendStatus(500);
+//         } else {
+//             var result = docs.map(function (data) {
+//                 return data;
+//             })
+//             res.json(result);
+//         }
+//     })
+// })
+
+// app.get("/pullOrder", function (req, res) {
+//     orders.find({
+//         "name": "ronaldo"
+//     })s.limit(1).next(function (err, docs) {
+//         if (err) {
+//             throw err;
+//             res.sendStatus(500);
+//         } else {
+
+//             res.json(docs);
+//         }
+//     })
+// })
+app.post("/pushOrder", (req, res) => {
+    orders.insert(req.body
+        , function (err, doc) {
+
+        });
+    console.log(req.body);
+
+})
+
+app.post('/animalPic', upload.single('animal'), function (req, res, next) {
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+  console.log("file:", req.file);
+})
+
+
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+    console.log({
+        "name": "art exhbit A",
+        "date": "tommorow"
+    })
 });
- 
-// Routes
- 
-    app.post('/api/machines', function(req, res) {
- 
-        Machine.find({
-            type: req.body.roomType,
-            beds: req.body.beds,
-            max_occupancy: {$gt: req.body.guests},
-            cost_per_night: {$gte: req.body.priceRange.lower, $lte: req.body.priceRange.upper},
-            reserved: { 
- 
-                //Check if any of the dates the room has been reserved for overlap with the requsted dates
-                $not: {
-                    $elemMatch: {from: {$lt: req.body.to.substring(0,10)}, to: {$gt: req.body.from.substring(0,10)}}
-                }
- 
-            }
-        }, function(err, rooms){
-            if(err){
-                res.send(err);
-            } else {
-                res.json(rooms);
-            }
-        });
- 
-    });
- 
-    app.post('/api/machines/reserve', function(req, res) {
- 
-        console.log(req.body._id);
- 
-        Machine.findByIdAndUpdate(req.body._id, {
-            $push: {"reserved": {from: req.body.from, to: req.body.to}}
-        }, {
-            safe: true,
-            new: true
-        }, function(err, room){
-            if(err){
-                res.send(err);
-            } else {
-                res.json(room);
-            }
-        });
- 
-    });
- 
-// listen
-app.listen(8080);
-console.log("App listening on port 8080");
